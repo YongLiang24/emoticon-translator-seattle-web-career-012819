eaf115756dada010789894d19a771a1b8395b13a
yong liang <yongliang24@gmail.com>
2019-01-24 07:19:25 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,5 +1,5 @@
 # require modules here
-=begin
+
 def load_library(directory)
   # code goes here
   file_name = YAML.load_file(directory)
@@ -8,22 +8,7 @@ def load_library(directory)
     data["get_meaning"][emo[1]] = definition
     data["get_emoticon"][emo[0]] = emo[1]
   end
-   data
-end
-=end
-def load_library(emoticon_file)
-  # code goes here	  emoticons = YAML.load_file('./lib/emoticons.yml')
-
-   emoticon_lib = {'get_meaning'  => {},
-                  'get_emoticon' => {} }
-
-   emoticons.each do |meaning, value|  # meaning= "surprised"   # value= [":o", "o_O"]  <-(e,j)
-    english = value[0]
-    japanese = value[1]
-    emoticon_lib['get_meaning'][japanese] = meaning
-    emoticon_lib['get_emoticon'][english] = japanese
-  end
-  emoticon_lib
+  return data
 end
 
 def get_japanese_emoticon
