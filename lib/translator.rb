9670bbf50fb1c0d6cdf6e744262d3b05c4827cf1
yong liang <yongliang24@gmail.com>
2019-01-24 07:12:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,14 @@
 # require modules here
 
-def load_library
+def load_library(file_path)
   # code goes here
+  library = YAML.load_file(file_path)
+  result = {"get_meaning" => {}, "get_emoticon" => {}}
+  library.each do |meaning, emoticons|
+    result["get_meaning"][emoticons[1]] = meaning
+    result["get_emoticon"][emoticons[0]] = emoticons[1]
+  end
+  result
 end
 
 def get_japanese_emoticon
