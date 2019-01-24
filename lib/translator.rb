1dddd915a8acbedcfb04820c3e3fa726ea7c1bad
yong liang <yongliang24@gmail.com>
2019-01-24 07:16:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,8 +4,8 @@ def load_library(directory)
   # code goes here
   file_name = YAML.load_file(directory)
   data = {"get_meaning" => {}, "get_emoticon" => {}}
-  file_name.each do |def, emo|
-    data["get_meaning"][emo[1]] = def
+  file_name.each do |definition, emo|
+    data["get_meaning"][emo[1]] = definition
     data["get_emoticon"][emo[0]] = emo[1]
   end
   return data
